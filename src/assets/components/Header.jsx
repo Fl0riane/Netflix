589749af4ebf92654cb04d0be23823bceb5b9aa1
Floriane Pelissier <flopel@MBP-de-Floriane.lan>
2023-04-25 14:12:02 +0200

    first commit

---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header>
+      <img
+        src="https://lereacteur-react-netflix.netlify.app/static/media/logo.696c2101.png"
+        alt="logo Netflix"
+      />
+    </header>
+  );
+};
+
+export default Header;
